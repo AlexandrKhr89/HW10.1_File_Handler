ddbd10d3a622000bfb312362ed2d257a8cb63533
Oleksandr Khrystenko <alexandrkhr89@gmail.com>
2026-06-24 17:40:56 +0300

    Solution

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -6,25 +6,24 @@ import java.io.IOException;
 
 public class FileHandler {
 
-    private final static String BASE_PATH = "files/";
-
-    public String writeFile(String fileContent) {
-        try(FileWriter fw = fileName + ".txt") {
-            fw(fileContent);
+    public String writeFile(String path, String fileName, String fileContent) {
+        try (FileWriter fw = new FileWriter(path + fileName + ".txt")) {
+            fw.write(fileContent);
             return "Success.";
         } catch (Exception e) {
-            return
+            System.out.println("Exception: " + e.getMessage());
+            return "Failed.";
         }
     }
 
     public String readFile(String path) {
-        try (FileReader reader = new FileReader()) {
+        try (FileReader reader = new FileReader(path)) {
             int sym;
-            new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             while ((sym = reader.read()) != -1) {
                 stringBuilder.append((char) sym);
             }
-            return
+            return stringBuilder.toString();
         } catch (IOException ex) {
             return ex.getMessage();
         }
